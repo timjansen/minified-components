a01a76cec79644bcf80e6546a1337dda97ced910
tim <tim@tjansen.de>
2014-05-12 19:46:34 +0200

    sync

--- src/animation.js
+++ src/animation.js
@@ -1,9 +1,7 @@
 define('niagara-animation', function(require) {
-	var NIAGARA = require('minified'); 
-  	var $ = NIAGARA.$, $$ = NIAGARA.$$, EE = NIAGARA.EE, _ = NIAGARA._;
+  var NIAGARA = require('minified'); 
+  var $ = NIAGARA.$, $$ = NIAGARA.$$, EE = NIAGARA.EE, _ = NIAGARA._;
 
-	// TODO:
-	// function smoothInterpolator(t, start, end, startVelocity, endVelocity);
 
 	/*$
 	 * Creates an interpolation function that will smoothly interpolate from the given start value and velocity
@@ -35,86 +33,135 @@ define('niagara-animation', function(require) {
 	// list.smoothDial({state1:0}, {state2:0}, {velocity1:0}, {velocity2: 0} )
 	// - if no velocity2, velocity1 is used for beginning and end
 	// - if no velocity1, velocity=0 is assumed
-	// - supports multi-property animation (like 'rotate(0 30 30)' to 'rotate(45 100 100)')
+	// - supports multi-property animation (like 'rotate(0 30 30)' to 'rotate(45 100 100)'). Velocity is then an array.
 
-	function extractNumbers(s) {
+	function extractInterpolatable(s) {
 		var nums = [], m;
-		while (m = /-?[0-9]+(?:\.[0-9]+)?/g.exec(s)) {
+		while (m = /(?:-?[0-9]+(?:\.[0-9]+)?)|(?:#\w{3}(?:\w{3})?)|(?:rgb\([^\)]+\))/g.exec(s))
 			nums.push(parseFloat(m[0]));
-		}
 		return nums;
 	}
 
-	function setNumbers(s, nums) {
+	function setInterpolatables(s, nums) {
 		var i = 0;
 		return s.replace(/-?[0-9]+(?:\.[0-9]+)?/, function() {
 			return nums[i++];
 		});
 	}
 
-	function dial(properties1, properties2, velocity1, velocity2) {
-		var self = this;
-		var v1 = velocity1, v2 = velocity2;
-		if (!v2)
-			v2 = v1; 
+    function extractSingleNumber(v) {
+        return parseFloat(replace(v, /^[^\d-]+/));
+    }
 
-		function getColorComponent(colorCode, index) {
-			return (/^#/.test(colorCode)) ?
-				parseInt(colorCode.length > 6 ? colorCode.substr(index*2+1, 2) : ((colorCode=colorCode.charAt(index+1))+colorCode), 16)
-				:
-				extractNumber(colorCode.split(',')[index]);
-		}
+    function getColorComponent(colorCode, index) {
+        return (/^#/.test(colorCode)) ?
+            parseInt(colorCode.length > 6 ? colorCode.substr(index*2+1, 2) : ((colorCode=colorCode.charAt(index+1))+colorCode), 16)
+        :
+            extractSingleNumber(colorCode.split(',')[index]);
+    }
+
+	function smoothDial(properties1, properties2, velocities1, velocities2) {
+		var self = this;
+
+        function getValues(properties) {
+            return _.mapObj(properties, function(name, start) {
+                return _(extractInterpolatable(start));
+            });   
+        }
+        var startValues = getValues(properties1);
+        var endValues = getValues(properties2);
+        function getVelocities(vmap) {
+            return _.mapObj(startValues, function(name, startValues) {
+                return startValues.map(function(startValue, index) {
+                    if (vmap && vmap[name] != null) {
+                        if (_.isList(vmap[name]))
+                            return vmap[name][index] || 0;
+                        else if (index == 0)
+                            return vmap[name];
+                    }
+                    else
+                        return 0;
+                });
+            });
+        }
+        var startVelocities = getVelocities(velocities1);
+        var endVelocities = getVelocities(velocities2 || velocities1);
+
 		return function(t) {
-			eachObj(properties1, function(name, start) {
-				var vStart = (v1 && v1[name]) || 0;
-				var vEnd = (v2 && v2[name]) || 0;
-				var end=properties2[name], i = 0; 
-				self.set(name, t<=0?start:t>=1?end: 
-					 (/^#|rgb\(/.test(end)) ? // color in format '#rgb' or '#rrggbb' or 'rgb(r,g,b)'?
-								('rgb('+ Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd)) 
-								+ ',' + Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd))
-								+ ',' + Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd))
-							    + ')')
-							:   // TODO: multi-prop
-								replace(end, /-?[\d.]+/, toString(smoothInterpolator(extractNumber(start), extractNumber(end), t, vStart, vEnd)))
-				);
+			_.eachObj(startValues, function(name, valueList) {
+                self.set(name, setInterpolatables(startValues[name], 
+                    valueList.map(function(start, valIndex) {
+                    var i = 0;
+                    var end = endValues[name][valIndex];
+                    var vStart = startVelocities[name][valIndex];
+                    var vEnd = endVelocities[name][valIndex];
+                    return t<=0?start:t>=1?end: 
+                        (/^#|rgb\(/.test(start)) ? // color in format '#rgb' or '#rrggbb' or 'rgb(r,g,b)'?
+                                    ('rgb('+ Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd)) 
+    								+ ',' + Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd))
+    								+ ',' + Math.round(smoothInterpolator(getColorComponent(start, i), getColorComponent(end, i++), t, vStart, vEnd))
+    							    + ')')
+    							: 
+    								smoothInterpolator(start, end, t, vStart, vEnd);
+                })));
 			});
 		};
 	}
 
+    function animateDial(dial, duration, promiseArguments) {
+        var prom = promise(); 
+        var durationMs = duration || 500;
+        var loopStop;
+
+        prom['stop0'] = function() { prom(false); return loopStop(); };
+
+        loopStop = $.loop(function(timePassedMs) {
+            dial(timePassedMs/durationMs);
+            if (timePassedMs >= durationMs) {
+                loopStop();
+                prom(true);
+            }
+        });
+        return prom;        
+    }
+
+
+	function smoothAnimate(properties1, properties2, velStart, velEnd, duration) {
+        return animateDial(this.smoothDial(properties1, properties2, velStart, velEnd), duration, [this]);
+    }
 
 
 
-	// TODO
-	// list.smoothAnimate({state1:0}, {state2:0}, {velocity1:0}, {velocity2: 0}, duration)
-	// - uses smoothDial()
-
-	// TODO
-	// timeline-based creation of interpolation functions, using smoothAnimator. 
+	// timeline-based creation of interpolation functions, using smoothInterpolator. 
 	// createInterpolator(listOfKeyFrames)
 	// var listOfKeyFrames= [ {t: time0to1, p: position0to1, v: velocity0To1} ]
 	//
-	function createInterpolator(listOfKeyFrames) {
-	     var keyFrames = _(listOfKeyFrames).array();
-	     if (keyFrames [0].t != 0)
-	          keyFrames.shift({t: 0, p: 0, v: 0});
-	     if (keyFrames[keyFrames.length-1].t != 1)
-	          keyFrames.push({t: 1, p: 1, v: 0});
-	     var fGen =  new Function("start", "d", "t", "subInterpolators", 
-	          "if (t <= 0) return start;\n"+
-	          "else if (t >= 1) return start+d;\n"+
-	          _(keyFrames).sub(1).map(function(kf, i) {
-	               return ' else if (t < '+(kf.t)+') return start+d*subInterpolators['+(i)+'];\n';
-	          }).join('')
-	     );
+    function createInterpolator(listOfKeyFrames) {
+        var keyFrames = _(listOfKeyFrames).array();
+        if (keyFrames [0].t != 0)
+            keyFrames.shift({t: 0, p: 0, v: 0});
+        if (keyFrames[keyFrames.length-1].t != 1)
+            keyFrames.push({t: 1, p: 1, v: 0});
+        var fGen =  new Function("start", "d", "t", "subInterpolators",
+            "if (t <= 0) return start;\n"+
+            "else if (t >= 1) return start+d;\n"+
+            _(keyFrames).sub(1).map(function(kf, i) {
+                return ' else if (t < '+(kf.t)+') return start+d*subInterpolators['+(i)+'](t);\n';
+            }).join('')
+        );
 
-	     var smoothInterpolators = []; // TODO: create funcs
+        var smoothInterpolators = _(keyFrames).sub(1).map(function(kf2, index) {
+            var kf1 = keyFrames[index];
+            var d = (kf2.t - kf1.t) || 1;
+            return function(t) {
+                return smoothInterpolator(kf1.p, kf2.p, (t - kf1.t) / d, kf1.v, kf2.v);
+            };
+        }).array();
 
-	     return function(start, end, t) {
-	          return fGen(start, end - start, t, smoothInterpolators);
-	     };
-	}
-
+        return function(start, end, t) {
+            return fGen(start, end - start, t, smoothInterpolators);
+        };
+    }
 
 
 	// TODO: 
@@ -175,7 +222,7 @@ define('niagara-animation', function(require) {
      //  - stop is an optional function that will be called by the timeline function when it was invoked with t>=duration
      // The timeline can be plugged right into $.loop(), or be used on its own.
      // Call the timeline function without arguments to make it return its duration.
-     function create(timelineDescriptor) {
+     function timeline(timelineDescriptor) {
           function processItem(tStart, e) {
                if (_.isList(e)) {
                     var ll = _.map(e, function(li) { return processItem(tStart, li); });
@@ -295,8 +342,14 @@ define('niagara-animation', function(require) {
                     stop();
           }
      }
+
+     NIAGARA.M.prototype.smoothDial = smoothDial;
+     NIAGARA.M.prototype.smoothAnimate = smoothAnimate;
    
-     return create;
+     return {
+        timeline: timeline,
+        animateDial: animateDial 
+     };
 });
 
 
